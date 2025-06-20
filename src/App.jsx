82c5e588d945a7f4014f21d98e75f766d7cdfd25
Kunal <thakurkunal369@gmail.com>
2025-06-20 12:09:02 +0530

    header - partially completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/Landing-Page/Header/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+      </>
+    ),
+  },
+]);
 
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={routes} />;
 };
 
 export default App;
--- src/components/Landing-Page/Header/Header.jsx
+++ src/components/Landing-Page/Header/Header.jsx
@@ -1,7 +1,131 @@
-import React from "react";
-
+import { NavLink } from "react-router-dom";
+import "./header.css";
+import { useState } from "react";
 const Header = () => {
-  return <div></div>;
+  const [feature, setFeature] = useState(false);
+  const [platforms, setPlatforms] = useState(false);
+  const pane = document.querySelector(".pane");
+
+  const handleFeatures = () => {
+    setPlatforms(false);
+    setFeature(!feature);
+    if (feature == true) {
+      pane.style.opacity = 1;
+    } else {
+      pane.style.opacity = 0;
+    }
+  };
+
+  const handlePlatform = () => {
+    setFeature(false);
+    setPlatforms(!platforms);
+    if (platforms == true) {
+      pane.style.opacity = 1;
+    } else {
+      pane.style.opacity = 0;
+    }
+  };
+
+  return (
+    <header className="header">
+      <section className="option-section">
+        <NavLink to={"/"} className="logo">
+          Logo
+        </NavLink>
+        <div className="features" onClick={handleFeatures}>
+          <p>Features</p>
+          <img src="src\assets\Drop_Down_Arrow.svg" />
+          <div className="pane">
+            {/* Windows */}
+            <div className="pane-items">
+              <img src="src\assets\windows.svg" alt="" />
+              <div className="pane-title">
+                <p>Windows</p>
+                <p className="p-text">Coming Soon..</p>
+              </div>
+            </div>
+
+            {/* Web */}
+            <div className="pane-items">
+              <img src="src\assets\web.svg" />
+              <div className="pane-title">
+                <p>Web Application</p>
+                <p className="p-text">Currently Using</p>
+              </div>
+            </div>
+
+            {/* Android */}
+            <div className="pane-items">
+              <img src="src\assets\android.svg" alt="" />
+              <div className="pane-title">
+                <p>Android</p>
+                <p className="p-text">Coming Soon..</p>
+              </div>
+            </div>
+
+            {/* iOS */}
+            <div className="pane-items">
+              <img src="src\assets\ios.svg" />
+              <div className="pane-title">
+                <p>Apple App</p>
+                <p className="p-text">Coming Soon</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="platforms" onClick={handlePlatform}>
+          <p>Platforms</p>
+          <img src="src\assets\Drop_Down_Arrow.svg" />
+          <div className="pane">
+            {/* Windows */}
+            <div className="pane-items">
+              <img src="src\assets\windows.svg" alt="" />
+              <div className="pane-title">
+                <p>Windows</p>
+                <p className="p-text">Coming Soon..</p>
+              </div>
+            </div>
+
+            {/* Web */}
+            <div className="pane-items">
+              <img src="src\assets\web.svg" />
+              <div className="pane-title">
+                <p>Web Application</p>
+                <p className="p-text">Currently Using</p>
+              </div>
+            </div>
+
+            {/* Android */}
+            <div className="pane-items">
+              <img src="src\assets\android.svg" alt="" />
+              <div className="pane-title">
+                <p>Android</p>
+                <p className="p-text">Coming Soon..</p>
+              </div>
+            </div>
+
+            {/* iOS */}
+            <div className="pane-items">
+              <img src="src\assets\ios.svg" />
+              <div className="pane-title">
+                <p>Apple App</p>
+                <p className="p-text">Coming Soon</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p id="about">About</p>
+      </section>
+
+      <section className="login-section">
+        <NavLink to={"/"} className="signin-btn">
+          Sign-in
+        </NavLink>
+        <button className="get-started-btn">GET STARTED</button>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
