91353b9c258a687447f7fcc972554fe0a61ed7d5
Kunal <thakurkunal369@gmail.com>
2025-06-26 11:02:01 +0530

    header changes

--- src/components/Landing-Page/Header/Header.jsx
+++ src/components/Landing-Page/Header/Header.jsx
@@ -2,22 +2,10 @@ import { NavLink } from "react-router-dom";
 import "./header.css";
 import { useState } from "react";
 const Header = () => {
-  const [feature, setFeature] = useState(false);
   const [platforms, setPlatforms] = useState(false);
   const pane = document.querySelector(".pane");
 
-  const handleFeatures = () => {
-    setPlatforms(false);
-    setFeature(!feature);
-    if (feature == true) {
-      pane.style.opacity = 1;
-    } else {
-      pane.style.opacity = 0;
-    }
-  };
-
   const handlePlatform = () => {
-    setFeature(false);
     setPlatforms(!platforms);
     if (platforms == true) {
       pane.style.opacity = 1;
@@ -32,46 +20,9 @@ const Header = () => {
         <NavLink to={"/"} className="logo">
           Logo
         </NavLink>
-        <div className="features" onClick={handleFeatures}>
+        <div className="features">
           <p>Features</p>
           <img src="src\assets\Drop_Down_Arrow.svg" />
-          <div className="pane">
-            {/* Windows */}
-            <div className="pane-items">
-              <img src="src\assets\windows.svg" alt="" />
-              <div className="pane-title">
-                <p>Windows</p>
-                <p className="p-text">Coming Soon..</p>
-              </div>
-            </div>
-
-            {/* Web */}
-            <div className="pane-items">
-              <img src="src\assets\web.svg" />
-              <div className="pane-title">
-                <p>Web Application</p>
-                <p className="p-text">Currently Using</p>
-              </div>
-            </div>
-
-            {/* Android */}
-            <div className="pane-items">
-              <img src="src\assets\android.svg" alt="" />
-              <div className="pane-title">
-                <p>Android</p>
-                <p className="p-text">Coming Soon..</p>
-              </div>
-            </div>
-
-            {/* iOS */}
-            <div className="pane-items">
-              <img src="src\assets\ios.svg" />
-              <div className="pane-title">
-                <p>Apple App</p>
-                <p className="p-text">Coming Soon</p>
-              </div>
-            </div>
-          </div>
         </div>
 
         <div className="platforms" onClick={handlePlatform}>
