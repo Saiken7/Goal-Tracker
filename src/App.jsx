172041543dd1690a44fee0d764c1dc51c61510e1
Saiken7 <saikenhere@gmail.com>
2025-07-06 11:24:21 +0530

    New Path Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from "./components/Landing-Page/Header/Header";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HeroSection from "./components/Landing-Page/Hero-Section/HeroSection";
+import SignupLoginHeader from "./components/Landing-Page/Header/SignupLoginHeader";
+import Signup from "./components/Main-Project/Signup/Signup";
 
 const routes = createBrowserRouter([
   {
@@ -9,6 +11,15 @@ const routes = createBrowserRouter([
       <>
         <Header />
         <HeroSection />
+      </>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <>
+        <SignupLoginHeader />
+        <Signup />
       </>
     ),
   },
