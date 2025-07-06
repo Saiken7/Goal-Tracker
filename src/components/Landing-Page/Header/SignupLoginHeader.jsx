bdd0fe9a31e458a5c361466d7b16aa3b4812decc
Saiken7 <saikenhere@gmail.com>
2025-07-06 11:23:44 +0530

    Signup/Login Page Header

---
+++ src/components/Landing-Page/Header/SignupLoginHeader.jsx
@@ -0,0 +1,11 @@
+import "./signupheader.css";
+
+const SignupLoginHeader = () => {
+  return (
+    <div className="signup-header">
+      <p>Logo</p>
+    </div>
+  );
+};
+
+export default SignupLoginHeader;
