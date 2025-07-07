eb28e94a7fd827d6cbd1e1947ee02203ec97d37c
Saiken7 <saikenhere@gmail.com>
2025-07-07 11:01:35 +0530

    Added Scroll Feature

--- src/components/Landing-Page/Hero-Section/HeroSection.jsx
+++ src/components/Landing-Page/Hero-Section/HeroSection.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import "./herosection.css";
-import {} from "../../../utils/scrollUtils.js";
 const HeroSection = () => {
   const navigate = useNavigate();
 
