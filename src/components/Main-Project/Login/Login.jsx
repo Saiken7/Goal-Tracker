e4861ee73d8186de4099e631f42ed1950cabc793
Saiken7 <saikenhere@gmail.com>
2025-07-07 10:43:14 +0530

    Added States and Values

--- src/components/Main-Project/Login/Login.jsx
+++ src/components/Main-Project/Login/Login.jsx
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import "../Signup/signup.css";
 import { Link } from "react-router-dom";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signup-main">
       <div className="container">
         <p id="upper-text">Think it. Make it.</p>
         <p id="sub-text">Login to your account</p>
-        <form className="form-box">
+        <form className="form-box" onSubmit={handleLoginSubmit}>
           <label className="label-text">Email</label>
           <input
-            type="mail"
+            type="email"
             placeholder="Enter your Email"
+            value={email || ""}
+            onChange={(e) => setEmail(e.target.value)}
             className="input-box"
           />
           <label className="label-text">Password</label>
           <input
             type="password"
             placeholder="Enter Password"
+            value={password || ""}
+            onChange={(e) => setPassword(e.target.value)}
             className="input-box"
           />
           <button className="signup-btn">Login</button>
