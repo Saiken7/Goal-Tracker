d28d3f625b281cc37da582e3a0b714e3c148e61e
Saiken7 <saikenhere@gmail.com>
2025-07-07 10:43:52 +0530

    Added States and Values

--- src/components/Main-Project/Signup/Signup.jsx
+++ src/components/Main-Project/Signup/Signup.jsx
@@ -1,35 +1,53 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./signup.css";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="signup-main">
       <div className="container">
         <p id="upper-text">Think it. Make it.</p>
         <p id="sub-text">Create new Account</p>
-        <form className="form-box">
+        <form className="form-box" onSubmit={handleSubmit}>
           <label className="label-text">Name</label>
           <input
             type="text"
             placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             className="input-box"
           />
           <label className="label-text">Email</label>
           <input
             type="mail"
             placeholder="Enter your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="input-box"
           />
           <label className="label-text">Phone</label>
           <input
             type="tel"
             placeholder="Enter your phone number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
             className="input-box"
           />
           <label className="label-text">Password</label>
           <input
             type="password"
             placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className="input-box"
           />
           <button className="signup-btn">Signup</button>
