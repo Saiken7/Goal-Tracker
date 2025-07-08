2661703ce18685c10a836deccac40ee2e66e1b33
Saiken7 <saikenhere@gmail.com>
2025-07-08 21:00:22 +0530

    Added Children Paths

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import HeroSection from "./components/Landing-Page/Hero-Section/HeroSection";
 import SignupLoginHeader from "./components/Landing-Page/Header/SignupLoginHeader";
 import Signup from "./components/Main-Project/Signup/Signup";
 import Login from "./components/Main-Project/Login/Login";
+import MainPage from "./components/Main-Project/MainPage/MainPage";
+import Dashboard from "../src/components/Main-Project/PageComponents/Dashboard";
+import Workspace from "../src/components/Main-Project/PageComponents/Workspace";
+import Settings from "../src/components/Main-Project/PageComponents/Settings";
+import Badges from "./components/Main-Project/PageComponents/Badges";
+import Calender from "./components/Main-Project/PageComponents/Calender";
 
 const routes = createBrowserRouter([
   {
@@ -32,6 +38,36 @@ const routes = createBrowserRouter([
         <Login />
       </>
     ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <>
+        <MainPage />
+      </>
+    ),
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        index: "workspace",
+        element: <Workspace />,
+      },
+      {
+        index: "settings",
+        element: <Settings />,
+      },
+      {
+        index: "badges",
+        element: <Badges />,
+      },
+      {
+        index: "calendar",
+        element: <Calender />,
+      },
+    ],
   },
 ]);
 
